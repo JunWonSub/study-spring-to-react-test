b510ca861117ed165a4f10abc5dca3270ee9ae4e
JunWonLaptop <wjdwnsdnjs13@naver.com>
2022-08-24 20:39:44 +0900

    first

---
+++ src/main/java/studyspringtoreact/studyspringtoreacttest/StudySpringToReactTestApplication.java
@@ -0,0 +1,13 @@
+package studyspringtoreact.studyspringtoreacttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringToReactTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringToReactTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/studyspringtoreact/studyspringtoreacttest/StudySpringToReactTestApplicationTests.java
@@ -0,0 +1,13 @@
+package studyspringtoreact.studyspringtoreacttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringToReactTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
